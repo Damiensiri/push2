18a0f4e4891c0c8efa5b301f6f6bef28166f2496
Damien SIRI <damiensiri@MacBook-Air-de-Damien.local>
2026-06-29 17:45:28 +0200

    Force le rafraîchissement des PWA installées

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,22 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+self.addEventListener("install",()=>{
+  self.skipWaiting();
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch",event=>{
+  const request=event.request;
+
+  if(request.method!=="GET" || request.mode!=="navigate") return;
+
+  event.respondWith(
+    fetch(request,{cache:"no-store"}).catch(async()=>{
+      return (await caches.match(request)) ||
+             (await caches.match("/index.html"));
+    })
+  );
+});
